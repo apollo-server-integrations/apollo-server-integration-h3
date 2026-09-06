40e9aed373787d094dc19e37dd8fd8fbbf095f84
Tobias Diez <code@tobiasdiez.de>
2026-09-07 00:42:52 +0200

    fix linter

--- src/index.ts
+++ src/index.ts
@@ -111,5 +111,7 @@ async function normalizeBody(event: H3Event): Promise<unknown> {
   const PayloadMethods: HTTPMethod[] = ['PATCH', 'POST', 'PUT', 'DELETE']
   if (isMethod(event, PayloadMethods)) {
     return await readBody(event)
+  } else {
+    throw new Error(`Unexpected HTTP method: ${event.req.method}`)
   }
 }
